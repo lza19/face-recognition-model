fc02eb882a57644d2cdfa2dd90c739e82e4d5fc0
lza19 <maxx5611056110@gmail.com>
2025-10-15 08:56:56 +0700

    8

--- test/bi.py
+++ test/bi.py
@@ -1,7 +1,16 @@
 import tensorflow as tf
 import sklearn
 from tensorflow.python.client import device_lib
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import os
+import matplotlib
+
 print("TensorFlow version:", tf.__version__)
 print("Scikit-learn version:", sklearn.__version__)
 print(tf.config.list_physical_devices('GPU'))
 print(device_lib.list_local_devices())
+print("NumPy version:", np.__version__)
+print("OpenCV version:", cv2.__version__)
+print("Matplotlib version:", matplotlib.__version__)
